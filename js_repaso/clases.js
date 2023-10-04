561c9f56980c6d5e7a4a36259c5ac0e69ce09579
jhordanjulcamoroms4m <jhordanjulcamoro@ms4m.com>
2023-10-04 09:39:43 -0500

    js - herencia

--- js_repaso/clases.js
+++ js_repaso/clases.js
@@ -14,6 +14,24 @@ class Vehiculo {
      }
 }
 
+class Autobus extends Vehiculo {
+    constructor(modelo, velocidad, antiguedad) {
+        super(modelo, velocidad, antiguedad);
+        this.altura = 5;
+    }
+
+    mostrarAltura(){
+        return "la altura del autobus es: " + this.altura;
+    }
+}
+
+var autobus1 = new Autobus("PEGASUS", 200, 2019);
+console.log("Autobus",autobus1);
+console.log("Autobus",autobus1.mostrarAltura());
+
+
+
+
 var vehiculo1 = new Vehiculo('BMW', 200, 2022);
 var vehiculo2 = new Vehiculo('AUDI', 250, 2021);
 var vehiculo3 = new Vehiculo('MERCES', 100, 2024);
